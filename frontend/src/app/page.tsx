8255f7536d1079b523c56687bc9edeb41187e507
yusei399 <yusei2021@keio.jp>
2024-03-16 04:58:42 +0900

    add stake dashboard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,32 +1,8 @@
 "use client";
-import { useEffect, useState } from 'react';
+import Dashboard from './components/Dashboard';
 
 export default function Home() {
-  const [userAddress, setUserAddress] = useState('');
-
-  useEffect(() => {
-    if (typeof window.ethereum !== 'undefined') {
-      try {
-        window.ethereum.request({ method: 'eth_requestAccounts' })
-          .then((accounts: string[]) => {
-            setUserAddress(accounts[0]);
-          });
-      } catch (error) {
-        console.error('Error connecting to MetaMask', error);
-      }
-    } else {
-      alert('MetaMask is not installed. Please consider installing it: https://metamask.io/download.html');
-    }
-  }, []);
-
   return (
-    <main>
-      {userAddress ? (
-        <p>Connected with: {userAddress}</p>
-      ) : (
-        <p>Not connected to MetaMask</p>
-      )}
-    </main>
+    <Dashboard/>
   );
 }
-//バーセルテスト
